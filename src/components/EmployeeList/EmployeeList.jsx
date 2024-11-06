d83613ce5ded42572ef2575ceb9b92fd6009f733
ShitalSapkota <shital.sapkota1@gmail.com>
2024-11-06 11:12:47 +0200

    add teachersData

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,31 +1,18 @@
 import EmployeeCard from "../EmployeeCard/EmployeeCard";
 import "./EmployeeList.css";
-
+import teachersData from "../../teachersData";
+import { useState } from "react";
 const EmployeeList = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const clickHandler = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
   return (
     <div>
-      <h2 className="list-title">List of the Employees</h2>
-      <EmployeeCard
-        name="Sara"
-        initialRole="Admin"
-        department="Management"
-        salary="6000"
-        location="Helsinki"
-      />
-      <EmployeeCard
-        name="Veera"
-        initialRole="Developer"
-        department="IT"
-        salary="4000"
-        location="Espoo"
-      />
-      <EmployeeCard
-        name="Joonas"
-        initialRole="Teacher"
-        department="ICT"
-        salary="5000"
-        location="Pasila"
-      />
+      <h2 className="list-title">List of the Employees (Teachers)</h2>
+      <main></main>
     </div>
   );
 };
---
+++ src/teachersData.js
@@ -0,0 +1,85 @@
+const teachersData = [
+    {
+        id: "1",
+        name: "Mary Jones",
+        role: "Backend Developer Teacher",
+        department: "Web Development",
+        start_date: "2016-05-12",
+        location: "Tampere"
+    },
+    {
+        id: "2",
+        name: "John Smith",
+        role: "Front-end Developer Teacher",
+        department: "Web Development",
+        start_date: "2013-04-01",
+        location: "Helsinki"
+    },
+    {
+        id: "3",
+        name: "Jane Doe",
+        role: "Designer",
+        department: "Web Development",
+        start_date: "2019-12-11",
+        location: "Pasila"
+    },
+    {
+        id: "4",
+        name: "Robert Johnson",
+        role: "APIs Developer",
+        department: "Backend",
+        start_date: "2020-02-13",
+        location: "Pasila"
+    },
+    {
+        id: "5",
+        name: "Mary Williams",
+        role: "Finnish Teacher",
+        department: "Humanities",
+        start_date: "2017-03-16",
+        location: "Turku"
+    },
+    {
+        id: "6",
+        name: "Michael Brown",
+        role: "Team Lead",
+        department: "ICT",
+        start_date: "2020-09-15",
+        location: "Pasila"
+    },
+    {
+        id: "7",
+        name: "William Jones",
+        role: "Admin",
+        department: "ICT",
+        start_date: "2019-07-28",
+        location: "Salo"
+    },
+    {
+        id: "8",
+        name: "James Wilson",
+        role: "Admin",
+        department: "ICT",
+        start_date: "2019-05-04",
+        location: "Pasila"
+    },
+    {
+        id: "9",
+        name: "Linda Taylor",
+        role: "Software Development Teacher",
+        department: "Web Development",
+        start_date: "2022-09-19",
+        location: "Helsinki"
+    },
+    {
+        id: "10",
+        name: "Emily Davis",
+        role: "UI/UX Design Teacher",
+        department: "Web designer",
+        start_date: "2016-07-28",
+        location: "Salo"
+    }
+
+];
+
+export default teachersData;
