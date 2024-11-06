75e27e0cc0b1a3371fbf5d224425e28c10bcbd45
ShitalSapkota <shital.sapkota1@gmail.com>
2024-11-06 14:52:55 +0200

    display teachers card and edit css

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -2,28 +2,32 @@ import "./EmployeeCard.css";
 import { useState } from "react";
 
 const EmployeeCard = (props) => {
-  const [role, setRole] = useState(props.initialRole);
+  const [role, setRole] = useState(props.role);
   // role is variable and setRole is method for changing value
 
   const clickButton = () => {
     //console.log("test")
-    if (role == props.initialRole) {
-      setRole("Team Lead");
+    if (role == props.role) {
+      setRole("Team Lead  ⭐");
     } else {
-      setRole(props.initialRole);
+      setRole(props.role);
     }
   };
 
+  /* Counting years of experience */
+
   return (
-    <div className="displayCards">
-      <div className="card">
-        <p>Name: {props.name}</p>
-        <p>Role: {role} </p>
-        <p>Department: {props.department} </p>
-        <p>Salary: {props.salary} </p>
-        <p>Location: {props.location} </p>
-        <button onClick={clickButton}>Promote</button>
-      </div>
+    <div className="card">
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Role: {role}</p>
+      <p>Department: {props.department} </p>
+      <p>Salary: {props.salary} </p>
+      <p>Start Date: {props.start_date} </p>
+      <p>Location: {props.location} </p>
+      <button onClick={clickButton}>
+        {role === "Team Lead  ⭐" ? "Demote" : "Promote"}
+      </button>
     </div>
   );
 };
--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -2,6 +2,7 @@ import EmployeeCard from "../EmployeeCard/EmployeeCard";
 import "./EmployeeList.css";
 import teachersData from "../../teachersData";
 import { useState } from "react";
+
 const EmployeeList = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -12,9 +13,38 @@ const EmployeeList = () => {
   return (
     <div>
       <h2 className="list-title">List of the Employees (Teachers)</h2>
-      <main></main>
+      <button onClick={clickHandler}>
+        {isLoggedIn ? "Log Out" : "Log In"}
+      </button>
+
+      <main className="card-container">
+        {isLoggedIn ? (
+          teachersData.map((teacher) => (
+            <div>
+              <EmployeeCard
+                key={teacher.id}
+                // key should be provided always when using list/array for dynamic value
+                {...teacher} // we can fetch all data at once like this
+                /*    name={teacher.name}
+                  age={teacher.age}
+                  role={teacher.role}
+                  department={teacher.department}
+                  salary={teacher.salary}
+                  start_date={teacher.start_date}
+                  location={teacher.location} */
+              />
+            </div>
+          ))
+        ) : (
+          <div>
+            <p>Please Log in to see the lists</p>
+          </div>
+        )}
+      </main>
     </div>
   );
 };
 
 export default EmployeeList;
+
+/* < EmployeeCard key= {employee.id} {...employee}> */
